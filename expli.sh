bae5d0c484612aff76041e35fb0c95618ea896c1
geekprod27 <86795495+geekprod27@users.noreply.github.com>
2022-09-11 19:17:51 +0200

    Update expli.sh

--- expli.sh
+++ expli.sh
@@ -1,5 +1,5 @@
 zenity --title "Tutors love u" --height=100 --width=300 --warning --text="Pense à lock ta session"
 zenity --title "Tutors love u" --height=100 --width=300 --warning --text="Pourquoi ?"
-zenity --title "Tutors love u" --height=100 --width=300 --question --text="Immagine en entreprise, tu as des documents confidentiels sur ta session, tu oublies de lock tq session et quelqu'un passe et vole les documents, tu pence que tu patron sera content ?"
-zenity --title "Tutors love u" --height=100 --width=300 --info --text="Maintenant voici un perroquet qui dance parce que c'est drole"
+zenity --title "Tutors love u" --height=100 --width=300 --question --text="Imagines en entreprise, tu as des documents confidentiels sur ta session, tu oublies de lock ta session et quelqu'un passe et vole les documents, tu penses que ton patron sera content ?"
+zenity --title "Tutors love u" --height=100 --width=300 --info --text="Maintenant voici un perroquet qui dance parce que c'est drôle"
 curl parrot.live
