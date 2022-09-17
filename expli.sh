2fd1b163cdf1e3dd59bbd7024160a353d958a796
geekprod27 <86795495+geekprod27@users.noreply.github.com>
2022-09-17 12:13:33 +0200

    explilock v2

--- expli.sh
+++ expli.sh
@@ -1,5 +1,8 @@
 zenity --title "Tutors love u" --height=100 --width=300 --warning --text="Pense à lock ta session"
-zenity --title "Tutors love u" --height=100 --width=300 --warning --text="Pourquoi ?"
-zenity --title "Tutors love u" --height=100 --width=300 --question --text="Imagines en entreprise, tu as des documents confidentiels sur ta session, tu oublies de lock ta session et quelqu'un passe et vole les documents, tu penses que ton patron sera content ?"
+zenity --title "Pourquoi ?" --height=100 --width=300 --question --text="Imagines en entreprise, tu as des documents confidentiels sur ta session, tu oublies de lock ta session et quelqu'un passe et vole les documents, tu penses que ton patron sera content ?"
+while [ $? -eq 0 ]
+do
+	zenity --title "Tu es sur qu'il/elle sera content(e) ?" --height=100 --width=300 --question --text="Imagines en entreprise, tu as des documents confidentiels sur ta session, tu oublies de lock ta session et quelqu'un passe et vole les documents, tu penses que ton patron sera content ?"
+done
 zenity --title "Tutors love u" --height=100 --width=300 --info --text="Maintenant voici un perroquet qui dance parce que c'est drôle"
 curl parrot.live
