be97f5ef8255087d17e6b7adc25a3d825c9d1260
geekprod27 <86795495+geekprod27@users.noreply.github.com>
2023-01-13 14:41:16 +0100

    Update boucleinf.sh

--- boucleinf.sh
+++ boucleinf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 while (true); do
-  zenity --title "Tutors love u" --height=100 --width=300 --warning --text="Pense à lock ta session"
+  zenity --title "Tutors love u" --height=100 --width=300 --warning --text="span color=\"red\"><span size=\"80000\">Pense à lock ta session</span></span>"
 done
