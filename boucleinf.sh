84f79a771af0eb2ca92a94dd365a7f2a4a99e7f7
Nathan FELSEMBERG <natfeljaf@gmail.com>
2024-02-06 16:46:36 +0100

    trad anglais

--- boucleinf.sh
+++ boucleinf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 while (true); do
-  zenity --title "Tutors love u" --height=100 --width=800 --warning --text="<span color=\"red\"><span size=\"80000\">Pense à lock ta session</span></span>"
+  zenity --title "Tutors love u" --height=100 --width=800 --warning --text="<span color=\"red\"><span size=\"80000\">Pense à lock ta session
+  Did you think about locking your session?</span></span>"
 done
--- expli.sh
+++ expli.sh
@@ -1,10 +1,18 @@
 var=0
-zenity --title "The Tutors team loves u" --height=100 --width=800 --warning --text="<span color=\"red\"><span size=\"80000\">Pense à lock ta session</span></span>"
-zenity --title "Pourquoi ?" --height=100 --width=300 --question --text="Imagines en entreprise:
+zenity --title "The Tutors team loves u" --height=100 --width=800 --warning --text="<span color=\"red\"><span size=\"75000\">Pense à lock ta session
+
+Did you think about locking your session?</span></span>"
+zenity --title "Pourquoi ? /  why ?" --height=100 --width=300 --question --text="Imagines en entreprise:
 tu as des documents confidentiels sur ta session...
 tu oublies de lock ta session et quelqu'un passe et vole les documents...
 
-Tu penses que ton patron sera content ?"
+Tu penses que ton patron sera content ?
+
+Imagine this scenario
+You have confidential documents in your session...
+You forgot to lock your session and now someone has stolen your documents...
+
+Do you think your boss will be happy?"
 while [ $? -eq 0 ]
 do
 	let "var++"
@@ -18,7 +26,14 @@ tu oublies de lock ta session et quelqu'un passe et vole les documents...
 
 Tu penses que ton patron sera content ?
 
-<span color=\"red\"><span size=\"$(($var * 5000))\">Il faut lire, pas regarder !</span></span>"
+Imagine this scenario
+You have confidential documents in your session...
+You forgot to lock your session and now someone has stolen your documents...
+
+Do you think your boss will be happy?
+
+<span color=\"red\"><span size=\"$(($var * 5000))\">Il faut lire, pas regarder !
+Just read, don't watch !</span></span>"
 		else
 		zenity --title "Tu es VRAIMENT sûr qu'il/elle sera content(e) ?" --height=100 --width=300 --question --text="Imagines en entreprise:
 tu as des documents confidentiels sur ta session...
@@ -26,14 +41,26 @@ tu oublies de lock ta session et quelqu'un passe et vole les documents...
 
 Tu penses que ton patron sera content ?
 
-<span size=\"$(($var * 5000))\">Il faut lire, pas regarder !</span>"
+Imagine this scenario
+You have confidential documents in your session...
+You forgot to lock your session and now someone has stolen your documents...
+
+Do you think your boss will be happy?
+
+<span size=\"$(($var * 5000))\">Il faut lire, pas regarder !
+Just read, don't watch !</span>"
 		fi
 	else
 		zenity --title "Tu es VRAIMENT sûr qu'il/elle sera content(e) ?" --height=100 --width=300 --question --text="Imagines en entreprise:
 tu as des documents confidentiels sur ta session...
 tu oublies de lock ta session et quelqu'un passe et vole les documents...
 
-Tu penses que ton patron sera content ?"
+Tu penses que ton patron sera content ?
+Imagine this scenario
+You have confidential documents in your session...
+You forgot to lock your session and now someone has stolen your documents...
+
+Do you think your boss will be happy?"
 	fi
 done
 zenity --title "The Tutors team loves u" --height=100 --width=300 --info --text="Allez retourne a tes projets !"
