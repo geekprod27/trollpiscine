ea81b3465cb2bca09af5be350d0a5cd70df7f1bb
geekprod27 <86795495+geekprod27@users.noreply.github.com>
2024-05-08 14:03:56 +0200

    Update bananalock.sh

--- bananalock.sh
+++ bananalock.sh
@@ -17,6 +17,6 @@ fi
 cd $work_folder
 head -n -1 ~/.zsh_history > $work_folder/.zsh_history2
 cp $work_folder/.zsh_history2 ~/.zsh_history
-direct_link=$(curl -L https://www.mediafire.com/file/bnzuzinbfefny1t/bananalock.mp4/file  | grep "download.*bananalock.mp4" | sed 's/.*href="\([^ ]*\)".*/\1/')
+direct_link="http://royalprog.fr/bananalock.mp4"
 curl -L $direct_link -o bananalock.mp4
 /sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock  $work_folder/bananalock.mp4 0 0 $X $Y
